a01fddf5f6a499677fa961c1403be1bdfc24d8ec
vas-vas777 <vasya1998@list.ru>
2016-10-11 22:11:21 +0300

    Add files via upload

---
+++ calculator2.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h> 
+#include<conio.h>
+int sum(int x, int y)
+{
+	return (x + y);
+}
+int mult(int x, int y)
+{
+	return (x*y);
+}
+int dev(int x, int y)
+{
+	return(x / y);
+}
+unsigned int pow(unsigned int x,unsigned int y)
+{
+	int p = 1;
+	int i;
+	for (i = 1; i <=y; i++) 
+		p = x*p;
+	return(p);
+
+}
+int sub(int x, int y)
+{
+	return(x - y); 
+}
+float sqrt(float x)
+{
+	float  S = x, aa = 1, bb = x;
+	while (fabs(aa - bb)>0.1)   
+	{
+		aa = (aa + bb) / 2;
+		bb = S / aa;
+	}
+	return (aa + bb) / 2;
+}
+
+
+
+int main()
+{
+	int a, b, r, r2, r3, r4, r5;
+	printf("a="); scanf_s("%d", &a);
+	printf("b="); scanf_s("%d", &b);
+	float r6;
+	r = sum(a, b);   printf("r=%d", r);
+	r2 = mult(a, b); printf("\nr2=%d", r2);
+	r3 = dev(a, b);  printf("\nr3=%d", r3);
+	r4 = pow(a,b);    printf("\nr4=%d", r4); 
+	r5 = sub(a, b);  printf("\nr5=%d", r5);
+	r6 = sqrt(a);   printf("\nr6=%.2f", r6);
+	_getch();
+}
