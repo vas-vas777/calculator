5619f7306251dd0a3a6820968a1cecb04ac6f980
vas-vas777 <vasya1998@list.ru>
2016-10-11 22:10:39 +0300

    Update Calculator1.cpp

--- Calculator1.cpp
+++ Calculator1.cpp
@@ -1,39 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h> 
 #include<conio.h>
-int sum(int a, int b)
+int sum(int x, int y)
 {
-	return (a + b);// считает сумму
+	return (x + y);
 }
-int mult(int a, int b)
+int mult(int x, int y)
 {    
-	return (a*b);// считает произведение
+	return (x*y);
 }
-int dev(int a, int b)
+int dev(int x, int y)
 {    
-	return(a/b);// считает деление 
+	return(x/y);
 }
-unsigned int pow( unsigned int a)
+unsigned int pow( unsigned int x)
 {  
 	int p=1, i,b=5;
-	for (i = 1; i <= b; i++) //вычисляет a в стпени b
-	p = a*p;
+	for (i = 1; i <= b; i++) 
+	p = x*p;
 	return(p);
 	
 }
-int sub(int a, int b)
+int sub(int x, int y)
 {
-	return(a-b); // вычисляет разность 
+	return(x-y); 
 }
 float sqrt(float x1) 
 {
-	float S = x1, a = 1, b = x1;
-	while (fabs(a - b)>0.1)  // квадратный корень из числа 
+	float S = x1, aa = 1, bb = x1;
+	while (fabs(aa - bb)>0.1)   
 	{ 
-	a = (a + b) / 2;
-	b = S / a; 
+	aa = (aa + bb) / 2;
+	bb = S / aa; 
 	}
-	return (a + b) / 2;
+	return (aa + bb) / 2;
 }
 
 
@@ -41,12 +41,12 @@ float sqrt(float x1)
 int main() 
 {
 int a=10, b=5,r,r2,r3,r4,r5;
-float r6,r7;
+float r6;
 r=sum(a,b);   printf("r=%d",r);
 r2=mult(a,b); printf("\nr2=%d",r2);
 r3=dev(a,b);  printf("\nr3=%d",r3);
-r4=pow(a);    printf("\nr4=%d",r4); // выводим полученные результаты 
+r4=pow(a);    printf("\nr4=%d",r4); 
 r5=sub(a,b);  printf("\nr5=%d",r5);
 r6=sqrt(a);   printf("\nr6=%.2f",r6);         
-getchar();      
+_getch();      
 }
