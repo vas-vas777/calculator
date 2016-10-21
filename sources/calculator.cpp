7bb8a7a5fb320e8d49878e31dc1a6907f0d1f966
vas-vas777 <vasya1998@list.ru>
2016-10-21 13:56:04 +0400

    Update calculator.cpp

--- sources/calculator.cpp
+++ sources/calculator.cpp
@@ -1,4 +1,35 @@
-int sum(int a, int b)
+int sum(int x, int y)
 {
-	return (a + b);
+	return (x + y);
+}
+int mult(int x, int y)
+{
+	return (x*y);
+}
+int dev(int x, int y)
+{
+	return(x / y);
+}
+int pow(int x, int y)
+{
+	int p = 1, i;
+	for (i = 1; i <= y; i++) 
+	{
+		p = p*x;
+	}
+	return(p);
+}
+int sub(int x, int y)
+{
+	return(x - y); 
+}
+unsigned sqrt(double xL)
+{
+	unsigned div = 1, rslt = 0;
+	while (x > 0)
+	{
+		x -= div, div += 2;
+		rslt += x < 0 ? 0 : 1;
+	}
+	return rslt;
 }
