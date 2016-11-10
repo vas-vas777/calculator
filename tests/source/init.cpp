a8c2fdf7bfbf9da62a6b19db7ccaaebcd93ca61e
vas-vas777 <vasya1998@list.ru>
2016-11-10 17:48:14 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,50 +2,50 @@
 #include <catch.hpp>
 
 SCENARIO("calculator sum", "[sum]") {
-    int x =4 ;
-    int y =1 ;
+    float x =4 ;
+    float y =1 ;
     
-    int rv = sum( x, y );
+    float rv = sum( x, y );
     
 	REQUIRE( rv ==5 );
 }
 SCENARIO("calculator mult", "[mult]") {
-    int x =4 ;
-    int y =1 ;
+    float x =4 ;
+    float y =1 ;
     
-    int rv = mult( x, y );
+    float rv = mult( x, y );
     
 	REQUIRE( rv ==4 );
 }
 SCENARIO("calculator dev", "[dev]") {
-    int x =4 ;
-    int y =1 ;
+    float x =4 ;
+    float y =1 ;
     
-    int rv = sum( x, y );
+    float rv = sum( x, y );
     
 	REQUIRE( rv ==4 );
 }
 SCENARIO("calculator pow", "[pow]") {
-    int x =4 ;
-    int y =1 ;
+    float x =4 ;
+    float y =1 ;
     
-    int rv = sum( x, y );
+    float rv = sum( x, y );
     
 	REQUIRE( rv ==4 );
 }
 SCENARIO("calculator sub", "[sub]") {
-    int x =4 ;
-    int y =1 ;
+    float x =4 ;
+    float y =1 ;
     
-    int rv = sum( x, y );
+    float rv = sum( x, y );
     
 	REQUIRE( rv ==3 );
 }
 SCENARIO("calculator sq", "[sq]") {
-    int x =4 ;
-    int y =1 ;
+    float x =4 ;
+    float y =1 ;
     
-    int rv = sq( x );
+    float rv = sq( x );
     
 	REQUIRE( rv ==2 );
 }
