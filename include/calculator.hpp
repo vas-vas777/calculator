d173d03b16b8cc8d5b741b152e4225f96d6e242b
vas-vas777 <vasya1998@list.ru>
2016-11-10 17:03:32 +0300

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -1,7 +1,7 @@
-int sum(int x, int y)
-int mult(int x, int y)
-int dev(int x, int y) 
-int pow(int x, int y)
-int sub(int x, int y) 
-unsigned sqrt(double xL)
+int sum(int x, int y);
+int mult(int x, int y);
+int dev(int x, int y) ;
+int pow(int x, int y);
+int sub(int x, int y) ;
+unsigned sqrt(double x);
   
