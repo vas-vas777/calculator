f9d8822afc32d9f998403e6e3d0d620b9114f224
vas-vas777 <vasya1998@list.ru>
2016-11-10 18:59:17 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,19 +1,20 @@
-#include<stdio.h>
-#include<stdlib.h> 
+#include <iostream>
 
-#include"calculator.hpp"
+#include "calculator.hpp"
+using namespace std;
 
 int main()
 {
-	float a, b, r, r2, r3,r4, r5;
-	printf_s("a="); scanf_s("%f", &a);
-	printf_s("b="); scanf_s("%f", &b);
-	float r6;
-	r = sum(a, b);   printf_s("r=%f", r);
-	r2 = mult(a, b); printf_s("\nr2=%f", r2);
-	r3 = dev(a, b);  printf_s("\nr3=%f", r3);
-	r4 = pow(a);    printf_s("\nr4=%f", r4); 
-	r5 = sub(a, b);  printf_s("\nr5=%f", r5);
-	r6 = sq(a);   printf_s("\nr6=%f", r6);
-	system("pause");
+    float a, b;
+    cin >> a;
+    cin >> b;
+    
+    cout << sum( a, b ) << endl;
+    cout << sub( a, b ) << endl;
+    cout << mult( a, b ) << endl;
+    cout << div( a, b ) << endl; 
+    cout << pow( a ) << endl;
+    cout << sq( a ) << endl;
+    
+    return 0;
 }
