e09bad4ff30c19ee23742581a29f320c4ef4d76e
vas-vas777 <vasya1998@list.ru>
2016-11-10 17:49:02 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -5,15 +5,15 @@
 
 int main()
 {
-	int a, b, r, r2, r3,r4, r5;
-	printf("a="); scanf_s("%d", &a);
-	printf("b="); scanf_s("%d", &b);
+	float a, b, r, r2, r3,r4, r5;
+	printf("a="); scanf_s("%f", &a);
+	printf("b="); scanf_s("%f", &b);
 	float r6;
-	r = sum(a, b);   printf("r=%d", r);
-	r2 = mult(a, b); printf("\nr2=%d", r2);
-	r3 = dev(a, b);  printf("\nr3=%d", r3);
-	r4 = pow(a,b);    printf("\nr4=%d", r4); 
-	r5 = sub(a, b);  printf("\nr5=%d", r5);
+	r = sum(a, b);   printf("r=%f", r);
+	r2 = mult(a, b); printf("\nr2=%f", r2);
+	r3 = dev(a, b);  printf("\nr3=%f", r3);
+	r4 = pow(a,b);    printf("\nr4=%f", r4); 
+	r5 = sub(a, b);  printf("\nr5=%f", r5);
 	r6 = sq(a);   printf("\nr6=%f", r6);
 	_getch();
 }
