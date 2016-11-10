613984b192ed74e8b4b5f7ce61b5e9c7e32f121a
vas-vas777 <vasya1998@list.ru>
2016-11-10 19:06:04 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,48 +2,48 @@
 #include <catch.hpp>
 
 SCENARIO("calculator sum", "[sum]") {
-    float x =4 ;
-    float y =1 ;
+    float x =4.0 ;
+    float y =1.0 ;
     
     float rv = sum( x, y );
     
 	REQUIRE( rv ==5.0 );
 }
 SCENARIO("calculator mult", "[mult]") {
-    float x =4 ;
-    float y =1 ;
+    float x =4.0 ;
+    float y =1.0 ;
     
     float rv = mult( x, y );
     
 	REQUIRE( rv ==4.0 );
 }
 SCENARIO("calculator dev", "[dev]") {
-    float x =4 ;
-    float y =1 ;
+    float x =4.0 ;
+    float y =1.0 ;
     
     float rv = dev( x, y );
     
 	REQUIRE( rv ==4.0 );
 }
 SCENARIO("calculator pow", "[pow]") {
-    float x =4 ;
-    float y =1 ;
+    float x =4.0 ;
+    float y =1.0 ;
     
     float rv = pow( x );
     
 	REQUIRE( rv ==16.0 );
 }
 SCENARIO("calculator sub", "[sub]") {
-    float x =4 ;
-    float y =1 ;
+    float x =4.0 ;
+    float y =1.0 ;
     
     float rv = sub( x, y );
     
 	REQUIRE( rv ==3.0 );
 }
 SCENARIO("calculator sq", "[sq]") {
-    float x =4 ;
-    float y =1 ;
+    float x =4.0 ;
+    float y =1.0 ;
     
     float rv = sq( x );
     
