4d311cb37274f7b9e6538295be6aaff3f363db2a
vas-vas777 <vasya1998@list.ru>
2016-11-17 17:57:38 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -16,7 +16,7 @@ int main()
         cout << "Forbidden" << endl;
     else    
     cout << dev( x, y ) << endl; 
-    cout << pow( x, y ) << endl;
+    cout << power( x, y ) << endl;
     if (x >= 0)
     cout << sq( x ) << endl;
     else
