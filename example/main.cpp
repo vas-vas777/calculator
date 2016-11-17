aef2b554f7263231bbf27e7f3c4fc38b73e3b1bb
vas-vas777 <vasya1998@list.ru>
2016-11-17 17:04:20 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -7,14 +7,19 @@ int main()
 {
     float x, y;
     cin >> x;
-    cin >> y;
+    cin >> y;      
     
     cout << sum( x, y ) << endl;
     cout << sub( x, y ) << endl;
     cout << mult( x, y ) << endl;
+    if (y == 0)
+        cout << "Forbidden" << endl;
+    else    
     cout << dev( x, y ) << endl; 
     cout << pow( x ) << endl;
+    if (a >= 0)
     cout << sq( x ) << endl;
-    
+    else
+    cout << "Forbidden" << endl;    
     return 0;
 }
