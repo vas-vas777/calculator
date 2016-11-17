5edf299ed789b70d4942b341412a1fdb30824049
vas-vas777 <vasya1998@list.ru>
2016-11-17 17:57:09 +0300

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -1,6 +1,6 @@
 float sum(float x, float y);
 float mult(float x, float y);
 float dev(float x, float y);
-float pow(float x, float y);
+float power(float x, float y);
 float sub(float x, float y);
 float sq(float x);
